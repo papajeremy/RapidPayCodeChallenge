4b49deca5e4cf65c1cabccfa84f5df6eb0fff88a
papajeremy <jeremy@bryantbergfamily.com>
2024-02-14 10:47:06 -0800

    clean up using statements removed migrations updated connection string

--- RapidPayApi/Controllers/AuthenticationController.cs
+++ RapidPayApi/Controllers/AuthenticationController.cs
@@ -1,5 +1,4 @@
-﻿using Azure;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using RapidPayApi.Authentication;
